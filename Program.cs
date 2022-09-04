c0eea585531c2e3b68a1094af3dc99877ff90aec
Adam Jeffries <adamljeffries@gmail.com>
2022-09-04 16:38:53 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TEST;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        bool open = true;
+
+        do
+        {
+            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine().ToLower();
+
+            if (input == "exit" )
+            {
+                open = false;
+            };
+
+        } while (open);
+    }
+}
